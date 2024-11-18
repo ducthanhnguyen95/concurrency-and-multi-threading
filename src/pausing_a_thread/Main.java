15efe37ede8734d15fa5baf0a9ea2721f2655528
thanhnd <ducthanhnguyen95@gmail.com>
2024-11-18 21:06:19 +0700

    Pausing a Thead

---
+++ src/pausing_a_thread/Main.java
@@ -0,0 +1,9 @@
+package pausing_a_thread;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        ThreadDemo.show();
+    }
+}
---
+++ src/pausing_a_thread/ThreadDemo.java
@@ -0,0 +1,12 @@
+package pausing_a_thread;
+
+public class ThreadDemo {
+    public static void show() {
+        System.out.println(Thread.currentThread().getName());
+
+        for (var i = 0; i < 10; i++) {
+            Thread thread = new Thread(new DownloadFileTask());
+            thread.start();
+        }
+    }
+}
