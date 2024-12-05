cb09e55418adb74dcf43cac35946099218a6f365
thanhnd <ducthanhnguyen95@gmail.com>
2024-12-06 00:20:16 +0700

    Adders

---
+++ src/adders/Main.java
@@ -0,0 +1,9 @@
+package adders;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        ThreadDemo.show();
+    }
+}
---
+++ src/adders/ThreadDemo.java
@@ -0,0 +1,25 @@
+package adders;
+
+import java.util.ArrayList;
+
+public class ThreadDemo {
+    public static void show() {
+        var status = new DownloadStatus();
+        var listThread = new ArrayList<Thread>();
+        for (var i = 0; i < 10; i++) {
+            var thread = new Thread(new DownloadFileTask(status));
+            thread.start();
+            listThread.add(thread);
+        }
+
+
+        for (Thread item : listThread) {
+            try {
+                item.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println(status.getTotalBytes());
+    }
+}
