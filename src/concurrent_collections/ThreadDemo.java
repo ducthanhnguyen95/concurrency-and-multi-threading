4fe5deb4a8df75a4218e2e81b49d3128c427a7bc
thanhnd <ducthanhnguyen95@gmail.com>
2024-12-06 22:28:52 +0700

    Concurrent collections

---
+++ src/concurrent_collections/ThreadDemo.java
@@ -0,0 +1,14 @@
+package concurrent_collections;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ThreadDemo {
+    public static void show() {
+        Map<Integer, String> map = new ConcurrentHashMap<>();
+        map.put(1, "a");
+        map.get(1);
+        map.remove(1);
+    }
+}
